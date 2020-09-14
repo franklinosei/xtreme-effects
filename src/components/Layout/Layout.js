7f29dc3a4eb65bc30645495dede9eb885e863532
franklinosei <franklinosei15@gmail.com>
2020-09-14 01:17:47 +0000

    Initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Layout = () => {
+    return(
+        <>
+            <h5>This is working!!!</h5>
+        </>
+    )
+}
+
+export default Layout;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../Layout/Layout';
+
+const Navbar = () => {
+    return(
+        <Layout>
+            <div></div>
+        </Layout>
+    )
+}
+
+export default Navbar;
